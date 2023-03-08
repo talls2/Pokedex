c93d5ce44f3cd1addf1f4ef77c50f38d4de71e5c
William Benett <williambenet@hotmail.com>
2023-03-08 17:59:58 -0300

    att 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
-var i = document.createElement("div");
-i.src = "https://pokeapi.co/api/v2/pokemon/35/";
-console.log(i.src);
+function convertPokemonToLi(pokemon) {
+  return `<li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+      <div class="details">
+        <ol class="types">
+          <li class="type">Grass</li>
+          <li class="type">Poison</li>
+        </ol>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+          alt="${pokemon.name}"
+        />
+      </div>
+    </li>
+    `;
+}
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi.getPokemons().then((pokemons) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+});
